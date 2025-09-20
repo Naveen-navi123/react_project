1c8f1f79b08fb37584deb29f482ff0d4bf934ef5
unknown <nr618358@gmail.com>
2025-09-19 17:18:51 -0700

    Add page

--- tech_shop/src/pages/Home.jsx
+++ tech_shop/src/pages/Home.jsx
@@ -7,7 +7,7 @@ import TopProducts from './TopProduct'
 const Home = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>Home Page </h1>
       <Carousel/>
       <Slider/>
       <TopProducts/>
